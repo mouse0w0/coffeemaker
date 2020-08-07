0465886c2c75b7b7db5edc0fde6808af48578576
Mouse <mouse0w0@qq.com>
2020-08-07 16:30:54 +0800

    Clean ASMUtils

--- src/main/java/com/github/mouse0w0/coffeemaker/util/ASMUtils.java
+++ src/main/java/com/github/mouse0w0/coffeemaker/util/ASMUtils.java
@@ -2,79 +2,11 @@ package com.github.mouse0w0.coffeemaker.util;
 
 
 import org.objectweb.asm.Type;
-import org.objectweb.asm.tree.AnnotationNode;
-import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class ASMUtils {
-
-    public static AnnotationNode getVisibleAnnotation(ClassNode clazz, Type type) {
-        return getAnnotation(clazz, type, false);
-    }
-
-    public static AnnotationNode getVisibleAnnotation(ClassNode clazz, String type) {
-        return getAnnotation(clazz, type, false);
-    }
-
-    public static AnnotationNode getInvisibleAnnotation(ClassNode clazz, Type type) {
-        return getAnnotation(clazz, type, true);
-    }
-
-    public static AnnotationNode getInvisibleAnnotation(ClassNode clazz, String type) {
-        return getAnnotation(clazz, type, true);
-    }
-
-    public static AnnotationNode getAnnotation(ClassNode clazz, Type type, boolean invisible) {
-        return getAnnotation(clazz, type.getDescriptor(), invisible);
-    }
-
-    public static AnnotationNode getAnnotation(ClassNode clazz, String type, boolean invisible) {
-        List<AnnotationNode> annotations = invisible ? clazz.invisibleAnnotations : clazz.visibleAnnotations;
-        if (annotations == null) {
-            return null;
-        }
-
-        for (AnnotationNode annotation : annotations) {
-            if (annotation.desc.equals(type)) {
-                return annotation;
-            }
-        }
-        return null;
-    }
-
-    public static MethodNode getMethod(ClassNode clazz, String name, String desc) {
-        for (MethodNode method : clazz.methods) {
-            if (method.name.equals(name) && method.desc.equals(desc)) {
-                return method;
-            }
-        }
-        return null;
-    }
-
-    public static Map<String, Object> getAnnotationValues(AnnotationNode annotation) {
-        Map<String, Object> valueMap = new HashMap<>();
-        List<Object> values = annotation.values;
-        for (int i = 0; i < values.size(); i += 2) {
-            valueMap.put((String) values.get(i), values.get(i + 1));
-        }
-        return valueMap;
-    }
-
-    public static <T> T getAnnotationValue(AnnotationNode annotation, String name) {
-        List<Object> values = annotation.values;
-        for (int i = 0; i < values.size(); i += 2) {
-            if (values.get(i).equals(name)) {
-                return (T) values.get(i + 1);
-            }
-        }
-        return null;
-    }
 
     public static String getMethodId(Class<?> clazz, String name, Class<?>... parameterTypes) {
         try {
@@ -86,10 +18,6 @@ public class ASMUtils {
 
     public static String getMethodId(Method method) {
         return Type.getInternalName(method.getDeclaringClass()) + "." + method.getName() + Type.getMethodDescriptor(method);
-    }
-
-    public static String getMethodId(MethodNode method) {
-        return method.name + method.desc;
     }
 
     public static String getMethodId(MethodInsnNode insnNode) {
