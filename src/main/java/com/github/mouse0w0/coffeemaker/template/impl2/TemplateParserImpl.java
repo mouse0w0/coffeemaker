8fc0e18f4eafadb78e3c4cad766ab03aa78f4452
Mouse <mouse0w0@qq.com>
2020-11-12 21:02:45 +0800

    Update impl2

--- src/main/java/com/github/mouse0w0/coffeemaker/template/impl2/TemplateParserImpl.java
+++ src/main/java/com/github/mouse0w0/coffeemaker/template/impl2/TemplateParserImpl.java
@@ -2,6 +2,7 @@ package com.github.mouse0w0.coffeemaker.template.impl2;
 
 import com.github.mouse0w0.coffeemaker.EmptyEvaluator;
 import com.github.mouse0w0.coffeemaker.template.*;
+import com.github.mouse0w0.coffeemaker.template.impl2.handler.Handler;
 import com.github.mouse0w0.coffeemaker.template.impl2.tree.BtClass;
 import com.github.mouse0w0.coffeemaker.template.impl2.tree.BtClassVisitor;
 import org.objectweb.asm.ClassReader;
@@ -11,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Handler;
 
 public class TemplateParserImpl implements TemplateParser {
     private static final String TEMPLATE_ANNOTATION = Type.getDescriptor(TemplateClass.class);
@@ -32,6 +32,10 @@ public class TemplateParserImpl implements TemplateParser {
 
         if (btClass.findAnnotation(TEMPLATE_ANNOTATION) == null) {
             throw new InvalidTemplateException(name.replace('/', '.'));
+        }
+
+        for (Handler handler : handlers) {
+            handler.handle(btClass);
         }
 
         return new TemplateImpl(name, btClass);
