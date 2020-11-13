89b166915d0d508656f7c7cb3baad7363e966430
Mouse <mouse0w0@qq.com>
2020-11-13 11:22:54 +0800

    Remove impl

--- src/main/java/com/github/mouse0w0/coffeemaker/template/impl/Processor.java
+++
@@ -1,8 +0,0 @@
-package com.github.mouse0w0.coffeemaker.template.impl;
-
-import com.github.mouse0w0.coffeemaker.evaluator.Evaluator;
-import com.github.mouse0w0.coffeemaker.template.impl.extree.ClassNodeEx;
-
-public interface Processor {
-    ClassNodeEx process(ClassNodeEx classNode, Evaluator evaluator);
-}
