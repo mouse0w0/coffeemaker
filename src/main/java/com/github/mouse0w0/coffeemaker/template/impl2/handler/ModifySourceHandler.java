611c8347259c25004b20b39262f2fab76520d95c
Mouse <mouse0w0@qq.com>
2020-11-15 00:44:53 +0800

    Fix ModifySourceHandler npe

--- src/main/java/com/github/mouse0w0/coffeemaker/template/impl2/handler/ModifySourceHandler.java
+++ src/main/java/com/github/mouse0w0/coffeemaker/template/impl2/handler/ModifySourceHandler.java
@@ -3,6 +3,7 @@ package com.github.mouse0w0.coffeemaker.template.impl2.handler;
 import com.github.mouse0w0.coffeemaker.template.ModifySource;
 import com.github.mouse0w0.coffeemaker.template.impl2.tree.BtAnnotation;
 import com.github.mouse0w0.coffeemaker.template.impl2.tree.BtClass;
+import com.github.mouse0w0.coffeemaker.template.impl2.tree.BtNode;
 import org.objectweb.asm.Type;
 
 public class ModifySourceHandler implements Handler {
@@ -14,8 +15,10 @@ public class ModifySourceHandler implements Handler {
         if (annotation != null) {
             clazz.getAnnotations().remove(annotation);
 
-            clazz.put(BtClass.SOURCE_FILE, annotation.getValues().get("sourceFile"));
-            clazz.put(BtClass.SOURCE_DEBUG, annotation.getValues().get("sourceDebug"));
+            BtNode sourceFile = annotation.getValues().get("sourceFile");
+            if (sourceFile != null) clazz.put(BtClass.SOURCE_FILE, sourceFile);
+            BtNode sourceDebug = annotation.getValues().get("sourceDebug");
+            if (sourceDebug != null) clazz.put(BtClass.SOURCE_DEBUG, sourceDebug);
         }
     }
 }
